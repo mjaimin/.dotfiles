9991365ee41c1518091d58406df8272ffb251685
Jaimin Modi <m.jaimin@gmail.com>
2020-08-21 10:34:37 +0530

    Edit

---
+++ .local/utilities.sh
@@ -0,0 +1,54 @@
+#
+# # ex - archive extractor
+# # usage: ex <file>
+ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1     ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+
+
+psgrep () {
+  ps aux | grep "$1" | grep -v "grep"
+}
+
+mkcd () {
+  mkdir -p "$1"
+  cd "$1"
+}
+cpbak () {
+  cp "$1"{,.bak}
+}
+
+
+# Find What is Using a Particular Port
+  # USAGE: $ whoisport 80
+whoisport () {
+        port=$1
+        pidInfo=$(fuser $port/tcp 2> /dev/null)
+        pid=$(echo $pidInfo | cut -d':' -f2)
+        ls -l /proc/$pid/exe
+}
+
+freemem () {
+    mem="$(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
+    echo -e "$mem ram "
+}
+
