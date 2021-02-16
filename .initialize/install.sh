435f1c241a5fe01d8a60a10b58012b234e6ecde2
mjaimin <m.jaimin@gmail.com>
2021-02-16 11:08:14 +0530

    .ditfile installation

---
+++ .initialize/install.sh
@@ -0,0 +1,8 @@
+apt-get install git
+mkdir -pv ~/.bash/bash-it/
+git clone https://github.com/mjaimin/bash-it.git ~/.bash/bash-it/
+git clone https://github.com/mjaimin/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/tmux-plugins/tmux-battery.git ~/.tmux/plugins/tmux-battery/
+#https://github.com/tmux-plugins/tmux-cpu.git
+#https://github.com/tmux-plugins/tmux-net-speed.git
+#https://github.com/tmux-plugins/tmux-copycat.git
